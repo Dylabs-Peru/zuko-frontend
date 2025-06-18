291bf6319d4d2e2bab2083d5cd318ad15f607120
PANCONLOCRO <rosamariarv10@gmail.com>
2025-06-18 18:36:23 -0500

    feat: crear model de usuario

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,36 @@
+
+
+export interface CreateUserRequest {
+  username: string;
+  email: string;
+  password: string;
+  url_image?: string;
+  description?: string;
+  roleName?: string;     // por defecto: "USER" si no se manda
+  isActive?: boolean;    // por defecto: true si no se manda
+}
+
+
+export interface UpdateUserRequest {
+  username?: string;
+  email?: string;
+  description?: string;
+  url_image?: string;
+  password?: string;
+  roleName?: string;
+}
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface UserResponse {
+  id: number;
+  username: string;
+  email: string;
+  description: string;
+  url_image: string;
+  roleName: string;
+  isActive: boolean;
+}
