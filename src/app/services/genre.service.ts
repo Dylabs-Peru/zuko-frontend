5877cac5ee65ce1fdbead24d8baec7e5fbce67ad
Turppie <codar2004@hotmail.com>
2025-06-19 21:48:52 -0500

    actualizar el genre.service para usar api.service

--- src/app/services/genre.service.ts
+++ src/app/services/genre.service.ts
@@ -1,50 +1,30 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { map, Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { GenreResponse, GenreRequest } from '../models/genre.model';
-import { environment } from '../../environments/environment';
+import { ApiService } from './Api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GenreService {
-  private baseUrl = `${environment.apiUrl}/genres`;
-  constructor(private http: HttpClient) {}
+  private endpoint = '/genres';
+
+  constructor(private api: ApiService) {}
 
   get_genres(): Observable<GenreResponse[]> {
-    const token = localStorage.getItem('token');
-    let headers = new HttpHeaders();
-    if (token) {
-      headers = headers.set('Authorization', `Bearer ${token}`);
-    }
-    return this.http.get<{data:GenreResponse[]}>(this.baseUrl, {headers}).pipe(map(response => response.data));
+    return this.api.get<{ data: GenreResponse[] }>(this.endpoint)
+      .pipe(map(response => response.data));
   }
 
   create_genre(genre: GenreRequest): Observable<GenreResponse> {
-    const token = localStorage.getItem('token');
-    let headers = new HttpHeaders();
-    if (token) {
-      headers = headers.set('Authorization', `Bearer ${token}`);
-    }
-    return this.http.post<GenreResponse>(this.baseUrl, genre, { headers });
+    return this.api.post<GenreResponse>(this.endpoint, genre);
   }
 
   update_genre(id: number, genre: GenreRequest): Observable<GenreResponse> {
-    const token = localStorage.getItem('token');
-    let headers = new HttpHeaders();
-    if (token) {
-      headers = headers.set('Authorization', `Bearer ${token}`);
-    }
-    return this.http.put<GenreResponse>(`${this.baseUrl}/${id}`, genre, { headers });
+    return this.api.put<GenreResponse>(`${this.endpoint}/${id}`, genre);
   }
 
   delete_genre(id: number): Observable<void> {
-    const token = localStorage.getItem('token');
-    let headers = new HttpHeaders();
-    if (token) {
-      headers = headers.set('Authorization', `Bearer ${token}`);
-    }
-    return this.http.delete<void>(`${this.baseUrl}/${id}`, { headers });
+    return this.api.delete(`${this.endpoint}/${id}`);
   }
-
 }
